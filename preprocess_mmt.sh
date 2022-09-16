b83d3fb059916ee1aa9fef05b0262f057360f173
Chuanhao Lv <38268730+zhulifengsheng@users.noreply.github.com>
2022-09-16 10:13:40 +0800

    Update preprocess_mmt.sh

--- preprocess_mmt.sh
+++ preprocess_mmt.sh
@@ -10,4 +10,4 @@ fairseq-preprocess --source-lang $src --target-lang $tgt \
   --testpref $TEXT/test.2016,$TEXT/test.2017,$TEXT/test.coco \
   --destdir data-bin/multi30k.en-$tgt.$mask \
   --workers 8 --joined-dictionary \
-  --srcdict data/dict.en2de_mask.txt
+  --srcdict data/dict.en2de_${mask}.txt
