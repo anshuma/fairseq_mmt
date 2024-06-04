9e1a38e43ddae6367bfb23f23255727a507eab38
Anshuma Shukla <anshumashuk@adobe.com>
2024-06-04 20:50:15 +0530

    Update blip cuda for efficiency

--- visual_bert/create_VBERT_from_pretrained_cuda.py
+++ visual_bert/create_VBERT_from_pretrained_cuda.py
@@ -51,101 +51,81 @@ tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 model = VisualBertForPreTraining.from_pretrained("uclanlp/visualbert-vqa-coco-pre").to(device)
 
 
-def preprocess_example(image_path, text):
-    visual_embeds = get_visual_embedding_blip(image_path)
-    visual_token_type_ids = torch.ones(visual_embeds.shape[:-1], dtype=torch.long, device=device)
-    visual_attention_mask = torch.ones(visual_embeds.shape[:-1], dtype=torch.float, device=device)
-
-    inputs = tokenizer(text, padding='max_length', truncation=True, return_tensors='pt').to(device)
-    labels = tokenizer(text, return_tensors="pt", padding="max_length",
-                       max_length=inputs["input_ids"].shape[-1] + visual_embeds.shape[-2])["input_ids"].to(device)
-
-    inputs.update({
-        "visual_embeds": visual_embeds,
-        "visual_token_type_ids": visual_token_type_ids,
-        "visual_attention_mask": visual_attention_mask,
-        "labels": labels.squeeze(0)
-    })
-    global count1
-    if (count1 == 0):
-        print('visual_embeds', visual_embeds.shape, flush=True)
-        print('visual_attention_mask.shape', visual_attention_mask.shape, flush=True)
-        print('attention_mask.shape', inputs['attention_mask'].shape, flush=True)
-    count1 = count1 + 1
-    print('count', count1, flush=True)
-    if (count1 % 50 == 0):
-        print('image_path', image_path, flush=True)
-        print('count1', count1, flush=True)
-
-    with torch.no_grad():
-        outputs = model(**inputs, output_hidden_states=True)
-    return outputs.hidden_states[-1]
-
-
 def load_and_preprocess_data(split):
     data = load_data(split)
-    outputs = []
-    if data is not None:
-        for index, row in data.iterrows():
-            inputs = preprocess_example(os.path.join(image_dir, f"{row['index']}"), row['caption'])
-            outputs.append(inputs)
-    return outputs
-
-
-def generate_and_save_predictions(encodings, split):
     global max_length
     tmp = []
     tmp1 = []
     global count
     model.eval()
     predictions = []
-    for inputs in encodings:
+    if data is not None:
         with torch.no_grad():
-            outputs = model(**inputs, output_hidden_states=True)
-            prediction_logits = outputs.prediction_logits
-            predicted_tokens = torch.argmax(prediction_logits, dim=-1)
-            predicted_sentence = tokenizer.decode(predicted_tokens[0], skip_special_tokens=True)
-            predictions.append(predicted_sentence)
-            last_layer_output = outputs.hidden_states[-1]
-            print('last_layer_output', last_layer_output.shape, flush=True)
-            tmp.append(last_layer_output.detach().to('device'))
-            if len(tmp) == 2000:
-                res = torch.cat(tmp)
-                print(res.shape, flush=True)
-                torch.save(res, os.path.join(output_dir, str(count) + split + '.pth'))
-                count += 1
-                tmp = []
+            for index, row in data.iterrows():
+                text = row['caption']
+                image_path = os.path.join(image_dir, f"{row['index']}")
+                visual_embeds = get_visual_embedding_blip(image_path)
+                visual_token_type_ids = torch.ones(visual_embeds.shape[:-1], dtype=torch.long, device=device)
+                visual_attention_mask = torch.ones(visual_embeds.shape[:-1], dtype=torch.float, device=device)
 
-    with open(os.path.join(output_dir, f"{split}_predictions.txt"), 'w') as f:
-        for prediction in predictions:
-            f.write(f"{prediction}\n")
+                inputs = tokenizer(text, padding='max_length', truncation=True, return_tensors='pt').to(device)
+                labels = tokenizer(text, return_tensors="pt", padding="max_length",
+                                   max_length=inputs["input_ids"].shape[-1] + visual_embeds.shape[-2])["input_ids"].to(
+                    device)
 
-    print('tmp', tmp, flush=True)
-    res = torch.cat(tmp).cpu()
-    if count > 1:
-        torch.save(res, os.path.join(output_dir, 'final' + split + '.pth'))
-    else:
-        print('feature shape:', res.shape, ',save in:', output_dir + '/' + split + '.pth', flush=True)
-        torch.save(res, os.path.join(output_dir, split + '.pth'))
+                inputs.update({
+                    "visual_embeds": visual_embeds,
+                    "visual_token_type_ids": visual_token_type_ids,
+                    "visual_attention_mask": visual_attention_mask,
+                    "labels": labels.squeeze(0)
+                })
+                global count1
+                if (count1 == 0):
+                    print('visual_embeds', visual_embeds.shape, flush=True)
+                    print('visual_attention_mask.shape', visual_attention_mask.shape, flush=True)
+                    print('attention_mask.shape', inputs['attention_mask'].shape, flush=True)
+                count1 = count1 + 1
+                print('count', count1, flush=True)
+                if (count1 % 50 == 0):
+                    print('image_path', image_path, flush=True)
+                    print('count1', count1, flush=True)
 
-    del tmp
-    _tmp = []
-    if count > 1:
-        for i in range(1, count):
-            _tmp.append(torch.load(os.path.join(output_dir, str(i) + split + '.pth')))
-        _tmp.append(torch.load(os.path.join(output_dir, 'final' + split + '.pth')))
-        res = torch.cat(_tmp).cpu()
-        print('feature shape:', res.shape, ',save in:', output_dir + '/' + split + '.pth', flush=True)
-        torch.save(res, os.path.join(output_dir, split + '.pth'))
+                outputs = model(**inputs, output_hidden_states=True)
+                last_layer_output = outputs.hidden_states[-1]
+                print('last_layer_output', last_layer_output.shape, flush=True)
+                tmp.append(last_layer_output.detach().to('device'))
+                if len(tmp) == 2000:
+                    res = torch.cat(tmp)
+                    print(res.shape, flush=True)
+                    torch.save(res, os.path.join(output_dir, str(count) + split + '.pth'))
+                    count += 1
+                    tmp = []
 
-        # delete
-        for i in range(1, count):
-            os.remove(os.path.join(output_dir, str(i) + split + '.pth'))
-        os.remove(os.path.join(output_dir, 'final' + split + '.pth'))
+            print('tmp', tmp, flush=True)
+            res = torch.cat(tmp).cpu()
+            if count > 1:
+                torch.save(res, os.path.join(output_dir, 'final' + split + '.pth'))
+            else:
+                print('feature shape:', res.shape, ',save in:', output_dir + '/' + split + '.pth', flush=True)
+                torch.save(res, os.path.join(output_dir, split + '.pth'))
+
+            del tmp
+            _tmp = []
+            if count > 1:
+                for i in range(1, count):
+                    _tmp.append(torch.load(os.path.join(output_dir, str(i) + split + '.pth')))
+                _tmp.append(torch.load(os.path.join(output_dir, 'final' + split + '.pth')))
+                res = torch.cat(_tmp).cpu()
+                print('feature shape:', res.shape, ',save in:', output_dir + '/' + split + '.pth', flush=True)
+                torch.save(res, os.path.join(output_dir, split + '.pth'))
+
+                # delete
+                for i in range(1, count):
+                    os.remove(os.path.join(output_dir, str(i) + split + '.pth'))
+                os.remove(os.path.join(output_dir, 'final' + split + '.pth'))
 
 
-# Similar modification for prediction function to utilize CUDA
 
 # Process and generate outputs for train, validation, and test sets
 train_outputs = load_and_preprocess_data('train')
-generate_and_save_predictions(train_outputs, 'train')
+#generate_and_save_predictions(train_outputs, 'train')
