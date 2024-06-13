cc78dd18231c6d52b1dfe5317f50157dde09156b
anshuma <anshuma.shukla@gmail.com>
2024-06-13 21:36:52 +0000

    fineTune_opus_mt-en-de_Image_EN_CustomModel.py

--- visual_bert/fineTune_opus_mt-en-de_Image_EN_CustomModel.py
+++ visual_bert/fineTune_opus_mt-en-de_Image_EN_CustomModel.py
@@ -34,9 +34,9 @@ def transform(
         **kwargs,
     )
 
-data_dir = '../small_dataset/data/multi30k-en-de'
-image_dir = '../small_dataset/flickr30k/flickr30k-images'
-image_idx_dir = '../small_dataset/flickr30k'
+data_dir = '../data/data/multi30k-en-de'
+image_dir = '../flickr30k/flickr30k-images'
+image_idx_dir = '../flickr30k'
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class ImageTextDataset(Dataset):
@@ -73,23 +73,24 @@ class BestModels:
         self.verbose = verbose
         self.best_models = []
 
-    def __call__(self, score, model, tokenizer, epoch):
+    def __call__(self, score, model, translation_model, tokenizer, epoch):
         if len(self.best_models) < self.k:
-            heapq.heappush(self.best_models, (score, epoch, model, tokenizer))
-            self.save_checkpoint(score, model, tokenizer, epoch)
+            heapq.heappush(self.best_models, (score, epoch, model, translation_model, tokenizer))
+            self.save_checkpoint(score, model, translation_model, tokenizer, epoch)
         else:
             if score > self.best_models[0][0]:  # Compare with the smallest score in the heap
-                _, _, old_model, old_tokenizer = heapq.heappop(self.best_models)
-                heapq.heappush(self.best_models, (score, epoch, model, tokenizer))
-                self.save_checkpoint(score, model, tokenizer, epoch)
+                _, _, old_model, old_translation_model, old_tokenizer = heapq.heappop(self.best_models)
+                heapq.heappush(self.best_models, (score, epoch, model, translation_model, tokenizer))
+                self.save_checkpoint(score, model, translation_model, tokenizer, epoch)
 
-    def save_checkpoint(self, score, model, tokenizer, epoch):
+    def save_checkpoint(self, score, translation_model, model, tokenizer, epoch):
         """Saves model when validation score improves."""
         path = f"best_model_epoch_{epoch}_score_{score:.4f}"
         if self.verbose:
             print(f"Validation score improved. Saving model to {path}...")
-        model.save_pretrained(path)
+        translation_model.save_pretrained(path)
         tokenizer.save_pretrained(path)
+        torch.save(model.state_dict(), path+'.pth')
 
 
 # Load models and tokenizers
@@ -245,10 +246,11 @@ def train_translation_model(model, clip_processor, text_model, text_tokenizer, t
                                     generation_config)
 
         # Save the best models
-        best_models(bleu_score.score, model.translation_model, model.translation_tokenizer, epoch)
+        best_models(bleu_score.score, model, model.translation_model, model.translation_tokenizer, epoch)
 
 
 # Define optimizer and scheduler
+from transformers import get_scheduler
 '''
 optimizer = torch.optim.Adam([
     {'params': model.translation_model.parameters()},
@@ -260,8 +262,14 @@ optimizer = torch.optim.Adam([
     {'params': [model.W1, model.W3]}
 ], lr=5e-5)
 num_training_steps = 30 * len(train_dataloader)  # Example number of epochs
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=num_training_steps // 3, gamma=0.1)
+num_epochs=30
+warmup_epochs = 8
+num_warmup_steps = (warmup_epochs / num_epochs) * num_training_steps
+#scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=num_training_steps // 3, gamma=0.1) #original
+scheduler = get_scheduler(
+            name="linear", optimizer=optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=num_training_steps
+            )
 
 # Train the model
 train_translation_model(model, clip_processor, text_model, text_tokenizer, train_dataloader, valid_dataloader,
-                        optimizer, scheduler, device, generation_config, num_epochs=30)
+                        optimizer, scheduler, device, generation_config, num_epochs=num_epochs)
